98c2453d89cb56b1e7a70f9c7b2aa8e8261b1806
Cory Booth <Rodimus2005@hotmail.com>
2018-05-17 08:50:50 -0500

    ummmm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,48 +1,71 @@
 import React, { Component } from 'react';
 
+const emptyUser = {
+  name: '',
+  city: '',
+  zip: '',
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: {
-        name: 'Cory',
-        city: 'Minneapolis'
-      }
+      user: emptyUser,
+
     };
-
-
-    // makes 'this' in handleChange the same as 'this' in the contructor
-    //this.handleChange = this.handleChange.bind(this);
-
   }
 
-  handleChange = (event) => {
-    console.log('event.target', event.target.value);
+  // makes 'this' in handleChange the same as 'this' in the contructor
+  //this.handleChange = this.handleChange.bind(this);  <-- old way!!!
 
-    //this.state.user = event.target.value;
-    this.setState({ name: event.target.value });
-  };
+  // }
+
+  // handleChange = (event) => {
+  //   console.log('event.target', event.target.value);
+
+  //this.state.user = event.target.value;
+  //   this.setState({
+  //     user: {
+  //       ...this.state.user,
+  //       name: event.target.value,
+  //     }
+  //   });
+  // };
 
 
-  cityChange = (event) => {
-    console.log('event.target', event.target.value);
 
-    //this.state.user = event.target.value;
-    this.setState({ city: event.target.value });
-  };
 
-  logPerson = () => {
+  handleChangeFor = propertyName => event => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [propertyName]: event.target.value,
+      }
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
     console.log(this.state);
-  };
+    this.setState({
+      user: emptyUser,
+    });
+  }
 
   render() {
     return (
       <div>
-        The current user is {this.state.user.name} and they are from {this.state.user.city}
-        <input onChange={this.handleChange} placeholder="User Name" />
-        <input onChange={this.cityChange} placeholder="City" />
-        <button onClick={this.logPerson}>CLICK MEH</button>
+        <form onSubmit={this.handleSubmit}>
+          The current user is {this.state.user.name} and they are from {this.state.user.city}
+          <input value={this.state.user.name} onChange={this.handleChangeFor('name')} placeholder="User Name" />
+          <input value={this.state.user.city} onChange={this.handleChangeFor('city')} placeholder="City" />
+          <input value={this.state.user.zip} onChange={this.handleChangeFor('zip')} placeholder="Zip Code" />
+          <input type="submit" value="click meh!" />
+        </form>
+        <ul>
+          {this.userArray = this.state.user.map(user => <li key={user.name}>name: {user.city} diameter: {user.zip}</li>)}
+        </ul>
       </div>
     );
   }
